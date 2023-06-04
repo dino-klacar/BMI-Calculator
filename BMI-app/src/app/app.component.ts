714b1b5078ef49ade0a6758247dbf31d1bc8ca69
Dino <dino.klacar@stu.ssst.edu.ba>
2023-06-04 19:28:55 +0200

    radi

--- BMI-app/src/app/app.component.ts
+++ BMI-app/src/app/app.component.ts
@@ -3,29 +3,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-
   title = 'BMIWebApp';
-  name: string = '';
   height!: number;
   weight!: number;
   bmi!: number;
   bmiCategory!: string;
+  Message!: string;
+  scales: { label: string }[] = [
+    { label: 'Underweight' },
+    { label: 'Normal weight' },
+    { label: 'Overweight' },
+    { label: 'Obese' },
+  ];
 
-  constructor() {
+  constructor() {}
 
-  }
-
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   calcBMI() {
     this.height = this.height / 100;
     this.bmi = Math.round(this.weight / (this.height * this.height));
     this.bmiCategory = this.determineBMICategory(this.bmi);
+    if (this.bmiCategory === 'Normal weight') {
+      this.Message = 'Keep up what you are doing!';
+    } else if (this.bmiCategory === 'Underweight') {
+      this.Message = 'You are underweight, you should gain some weight';
+    } else if (this.bmiCategory === 'Overweight') {
+      this.Message = 'You are overweight, you should lose some weight';
+    } else {
+      this.Message = 'You are obese,You should contact your doctor!';
+    }
   }
 
   determineBMICategory(bmi: number): string {
@@ -37,6 +47,18 @@ export class AppComponent implements OnInit {
       return 'Overweight';
     } else {
       return 'Obese';
+    }
+  }
+
+  calculateIndicatorPosition(): number {
+    if (this.bmiCategory === 'Underweight') {
+      return 0;
+    } else if (this.bmiCategory === 'Normal weight') {
+      return 33;
+    } else if (this.bmiCategory === 'Overweight') {
+      return 66;
+    } else {
+      return 100;
     }
   }
 }
